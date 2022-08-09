3dbeec57ae10889cacf23f421176d3520ff19789
Sergey Volgin <artphoto@mail.ru>
2022-08-09 13:02:21 +0300

    Add task 2

--- Task_02/Program.cs
+++ Task_02/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+double numberA = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double numberB = double.Parse(Console.ReadLine());
+
+if (numberA > numberB) 
+    Console.WriteLine("Число " + numberA + " больше, чем " + numberB);
+else if (numberA < numberB) 
+    Console.WriteLine("Число " + numberB + " больше, чем " + numberA);
+else if (numberA == numberB) 
+    Console.WriteLine("Числа равны");
+else 
+    Console.WriteLine("Введено не число");
