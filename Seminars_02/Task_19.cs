9c9bfabe340c07798818568c5b511d94580214b9
SnovvBars <110028220+SnovvBars@users.noreply.github.com>
2022-08-19 00:54:56 +0300

    Update Task_19.cs

--- Seminars_02/Task_19.cs
+++ Seminars_02/Task_19.cs
@@ -1,1 +1,20 @@
+// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
 
+void checks(string num)
+{
+    if (num[0] != num[4] || num[1] != num[3]) 
+        {
+            Console.WriteLine("Введенное число не полиндром");
+        }
+    else Console.WriteLine("Введенное число полиндром");
+    return;
+} 
+
+Console.WriteLine("Введите пятизначное число:");
+string polindrom = Console.ReadLine();
+
+checks(polindrom);
